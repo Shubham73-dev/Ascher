03a3bc471535bb4f3ff38aaa2c117502b4a5bdd5
shubham shrivastav <shubhamshrivastav16@gmail.com>
2023-06-02 15:47:59 +0530

    fixing for the second carousel

--- scripts/main.js
+++ scripts/main.js
@@ -8,6 +8,7 @@ function addToggleEvent() {
         togglers[i].addEventListener('click', toggleSlide);
     }
 }
+addToggleEvent(); // calling of addToggleEvent
 
 function toggleSlide() {
     for (var i = 0; i < togglers.length; i++) {
@@ -20,6 +21,7 @@ function toggleSlide() {
     currentIndex = index; // Update the current index
 }
 
+// method to animate first crousel starts from here
 function animateSlide() {
     currentIndex = (currentIndex + 1) % slidePaths.length; // Increment the current index and wrap around if necessary
     bannerEle.style.backgroundImage = 'url(' + slidePaths[currentIndex] + ')';
@@ -30,8 +32,39 @@ function animateSlide() {
     }
     togglers[currentIndex].style.backgroundColor = 'white';
 }
+// method to animate first crousel ends here
 
-addToggleEvent();
 
-// Automatically animate the slide every 0.5 seconds
+// Automatically animate the slide every 0.5 seconds fro first-carousel
 setInterval(animateSlide, 6000);
+
+// script for OWL carousel starts here
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+      items: 4, // Number of cards to be visible at a time
+      loop: true, // Enable loop
+      margin: 10, // Margin between cards
+      nav: false, // Disable default navigation buttons
+      responsive: {
+        0: {
+          items: 1 // Number of cards to be visible on small screens (optional)
+        },
+        576: {
+          items: 2 // Number of cards to be visible on medium screens (optional)
+        },
+        992: {
+          items: 4 // Number of cards to be visible on large screens (optional)
+        }
+      }
+    });
+
+    // Custom navigation buttons
+    $('.custom-next').click(function () {
+      $('.owl-carousel').trigger('next.owl.carousel');
+    });
+    $('.custom-prev').click(function () {
+      $('.owl-carousel').trigger('prev.owl.carousel');
+    });
+  });
+
+// script for OWL carousel ends here
